54a16ecc27558b2ddf6e9d28916bfbf8088278e8
JOlths <71876557+JOlths@users.noreply.github.com>
2023-03-24 12:31:09 +0100

    Updated (and finished) practical

--- Practical_2_Model_Validation.R
+++ Practical_2_Model_Validation.R
@@ -3,6 +3,8 @@ library(readxl)
 library(dplyr)
 library(magrittr)
 library(ggplot2)
+library(rms)
+library(rmda)
 
 ##load dataset ####
 covidpredict <- readxl::read_excel('covidpredict-1.xlsx')
@@ -17,11 +19,7 @@ covidpredict.training <- covidpredict %>%
 
 covidpredict.training %<>% select(., -set)
 
-#change binary/categorical variables to their respective datatype
-covidpredict.training$female %<>% as.factor()
-covidpredict.training$comorbidity %<>% as.factor()
-covidpredict.training$mortality %<>% as.factor()
-covidpredict.training$date %<>% as.Date()
+
 
 #check for missing values
 sapply(covidpredict.training, function(x) sum(is.na(x)))
@@ -30,10 +28,48 @@ sapply(covidpredict.training, function(x) sum(is.na(x)))
 sapply(covidpredict.training, function(x) length(x))
 
 # model training
-glm.covidpredict <- glm(data = covidpredict.training,
+lrm.covidpredict <- lrm(data = covidpredict.training,
                         formula = mortality ~ female + comorbidity + rr +
-                          oxygen_sat + urea + crp + gcs + age,
-                        family = binomial)
-summary(glm.covidpredict)
+                          oxygen_sat + urea + crp + gcs + age + age*comorbidity, x=T, y=T)
 
-## Assignment 1 ####
+## Exercise 1 ####
+rms::validate(lrm.covidpredict, method = 'boot', B=40)
+
+#apparent calibration slope: 1.00
+# optimism correced calibration slope: 0.9831
+# most reliable performance with new patients: optimism corrected slope
+
+## Exercise 2 ####
+C_stat <- function(Dxy) {return(0.5*(Dxy+1))}
+C_stat(0.5458) #original C-statistic
+C_stat(0.5399) #optimism corrected C-statistic. This one gives the best performance with new patient inputs
+
+## Exercise 3 ####
+res <- rms::calibrate(lrm.covidpredict, B=200)
+plot(res)
+ #decent prediction, some under and overprediction but in general quite decent
+
+## Assignment 2 ####
+covidpredict.test <- covidpredict %>%
+  dplyr::filter(set == 'val') %>%
+  as.data.frame()
+
+head(covidpredict.test)
+
+
+predictions <- stats::predict(lrm.covidpredict, covidpredict.test, type='fitted')
+head(predictions)
+
+validation <- rms::val.prob(predictions, covidpredict.test$mortality, g=10)
+
+## Assignment 3 ####
+covidpredict.test$prediction <- predictions
+head(covidpredict.test$prediction)
+
+dca <- rmda::decision_curve(data = covidpredict.test,
+                            formula = mortality ~ prediction,
+                            fitted.risk = T)
+rmda::plot_decision_curve(dca)
+
+summary(dca, measure="TPR")
+summary(dca, measure="FPR")
