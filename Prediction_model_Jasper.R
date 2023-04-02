330646ad5481fba149dd829155497148ca749b4c
JOlths <71876557+JOlths@users.noreply.github.com>
2023-04-02 17:13:58 +0200

    Update preprocessing

--- Prediction_model_Jasper.R
+++ Prediction_model_Jasper.R
@@ -16,5 +16,25 @@ data$papflow %<>% as.factor()
 data$wallreg %<>% as.factor()
 data$Shadows %<>% as.factor()
 data$pershistovca %<>% as.factor()
-data$famhistovca %<>% as.factor()
+data$famhistovca <- as.factor(ifelse(data$famhistovca == 0, 0, ifelse(data$famhistovca == 1, 1, 2)))
+data$hormtherapy %<>% as.factor()
+data$pain %<>% as.factor()
+data$colscore %<>% as.factor()
+data$nrloculescat %<>% as.factor()
+data$papnr %<>% as.factor()
+data$oncocenter %<>% as.factor()
+data$mal %<>% as.factor()
 
+## Model building ####
+lg_model.1 <- rms::lrm(
+  data = data,
+  formula = mal ~ .,
+  x = T, y = T
+)
+
+lg_model.1
+
+#validate
+tab <- rms::validate(lg_model.1, method = 'boot', B=40)
+C_stat <- function(Dxy) {return(0.5*(Dxy+1))}
+sapply(tab[1,], C_stat)
